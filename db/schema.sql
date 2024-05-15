c157c00213a1bb91952c03f2aa0ce57da8bcd5b7
Hunter <hunterde91@gmail.com>
2024-05-14 21:30:30 -0400

    fixed schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
--- Drop and create database
-DROP DATABASE IF EXISTS tech_blog_db;
-CREATE DATABASE tech_blog_db;
+-- Drops database --
+DROP DATABASE IF EXISTS techblog_db;
+
+-- Creates database --
+CREATE DATABASE techblog_db;
+
+-- Uses the techblog_db database –
+\c techblog_db;
